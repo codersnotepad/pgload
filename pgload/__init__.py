df94a04f7953701519b5a7841d225344af894ef1
timotewb <timote.wb@gmail.com>
2020-06-13 09:11:02 +0100

    version update for deployment

--- pgload/__init__.py
+++ pgload/__init__.py
@@ -17,5 +17,6 @@ del hard_dependencies, dependency, missing_dependencies
 from .pgload import pgload
 
 pgload = pgload()
+__version__ = "0.2.2"
 # validate_data = pgload().validate_data
 # scd2_load = pgload().scd2_load
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requirements = ["psycopg2>=2.8"]
 
 setup(
     name="pgload",
-    version="0.2.1",
+    version="0.2.2",
     author="Timote WB",
     author_email="timote.wb@gmail.com",
     description="Library of tools to load data from the web into PostgreSQL ",
