91e29ef73a44bb1c4ea66f23d9d3d41dae97c50e
timotewb <timote.wb@gmail.com>
2020-10-04 13:21:24 +0100

    upgrade to 0.2.6

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requirements = ["psycopg2>=2.8"]
 
 setup(
     name="pgload",
-    version="0.2.5",
+    version="0.2.6",
     author="Timote WB",
     author_email="timote.wb@gmail.com",
     description="Library of tools to load data from the web into PostgreSQL ",
